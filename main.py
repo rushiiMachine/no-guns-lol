dfcfa6199176c15a0ee1ccfb1e621aa11226adb2
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2025-07-12 20:42:51 -0700

    feat: allow whitelisting specific users

--- main.py
+++ main.py
@@ -36,10 +36,11 @@ async def handle_member(member: Member) -> bool:
 
 
 class NoGunsLolClient(Client):
-    def __init__(self, target_guilds: list[int]):
+    def __init__(self, target_guilds: list[int], whitelist_users: list[int]):
         super(NoGunsLolClient, self).__init__(guild_subscriptions=False, max_messages=None)
         self._all_target_guilds: set[int] = set(target_guilds)
         self._available_target_guilds: set[int] = set()
+        self._whitelist_users: set[int] = set(whitelist_users)
 
     # Events
 
@@ -64,8 +65,9 @@ class NoGunsLolClient(Client):
             await guild.subscribe(typing=True, activities=False, threads=False, member_updates=True)
 
     async def on_member_join(self, member: Member):
-        profile = await member.profile()
-        await handle_member(profile)
+        if member.id not in self._whitelist_users:
+            profile = await member.profile()
+            await handle_member(profile)
 
     async def on_message(self, message: Message):
         if message.author.id != self.user.id: return
@@ -78,7 +80,7 @@ class NoGunsLolClient(Client):
             await message.reply(f"Scanning {len(members)} members, estimated time: {time_estimate}")
 
             members_banned = 0
-            for member in members:
+            for member in [m for m in members if m.id not in self._whitelist_users]:
                 sleep(1.01)
                 members_banned += await handle_member(member)
 
@@ -88,9 +90,11 @@ class NoGunsLolClient(Client):
 def main():
     token = os.getenv("DISCORD_TOKEN")
     guilds_raw = os.getenv("GUILDS")
+    users_whitelist = os.getenv("WHITELIST_USERS")
     debug = len(os.getenv("DEBUG", "")) > 0
 
     guilds = []
+    whitelist_users = []
 
     if not token:
         raise Exception("Missing DISCORD_TOKEN environment variable)")
@@ -99,9 +103,17 @@ def main():
         _log.warning("The GUILDS environment variable is missing, not subscribing to any guilds...")
         _log.warning("This means that no new users from any guild will be scanned")
     else:
-        guilds = [int(uid) for uid in guilds_raw.split(",")]
+        guilds = [int(gid) for gid in guilds_raw.split(",")]
 
-    NoGunsLolClient(target_guilds=guilds).run(
+    if users_whitelist:
+        whitelist_users = [int(uid) for uid in users_whitelist.split(",")]
+        _log.info(f"Users exempted from bans: {whitelist_users}")
+
+    client = NoGunsLolClient(
+        target_guilds=guilds,
+        whitelist_users=whitelist_users)
+
+    client.run(
         token,
         root_logger=True,
         log_level=logging.DEBUG if debug else logging.INFO)
